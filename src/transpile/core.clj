9d41fbd3dfa2d5283092d335708167c4cec286bb
Libor Danek <lbr.dnk@posteo.net>
2023-09-13 12:39:48 +0200

    Use macro expansion in `generate-sql`

--- src/transpile/core.clj
+++ src/transpile/core.clj
@@ -4,8 +4,12 @@
    [transpile.sql]
    [transpile.postgres]
    [transpile.mysql]
-   [transpile.sqlserver]))
+   [transpile.sqlserver]
+   [transpile.util :as util]))
 
 (defn generate-sql
-  [dialect fields query]
-  (dialect/generate-sql dialect fields query))
+  ([dialect fields query]
+   (dialect/generate-sql dialect fields query))
+  ([dialect fields query macros]
+   (dialect/generate-sql dialect fields
+                         (update query :where #(util/expand-macros % macros)))))
